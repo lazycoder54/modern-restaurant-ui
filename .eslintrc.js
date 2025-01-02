34bedfcd46aa7c753bc3d9dcd284442a17e44700
Pranjit Das <pranjitd865@gmail.com>
2025-01-02 20:57:10 +0530

    second commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,9 @@ module.exports = {
     },
     ecmaVersion: 2018,
     sourceType: 'module',
+    requireConfigFile: false,
   },
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   plugins: [
     'react',
   ],
